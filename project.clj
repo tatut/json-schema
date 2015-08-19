03b264fd75b81ce4b8c3872c9afe0c739da70401
Tatu Tarvainen <tatu.tarvainen@me.com>
2015-08-19 20:48:32 +0300

    Add description/url/license

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject webjure/json-schema "0.1"
+(defproject webjure/json-schema "0.1.1"
+  :description "Minimalistic JSON schema validator with $ref support."
+  :url "https://github.com/tatut/json-schema"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]]
 
