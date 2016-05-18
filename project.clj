43738c6e8b72428f864eb88ff27e0eec85dbdb3e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-18 21:45:53 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url  "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.5.0"]
-                 [clj-time "0.10.0"]]
+                 [cheshire "5.6.1"]
+                 [clj-time "0.11.0"]]
   :source-paths ["src"]
   :test-paths ["test"])
