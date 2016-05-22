fe2784eab09c4d44b4cee127236aecc10a134963
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-22 21:37:00 +0300

    Bump version to 0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webjure/json-schema "0.7"
+(defproject webjure/json-schema "0.7.1"
   :description "Minimalistic JSON schema validator with $ref support."
   :url "https://github.com/tatut/json-schema"
   :license {:name "MIT License"
